459a3bc5a6b3bdc9cea8365aa5dd6355591f6183
Maciej Urbański <maciek@elotl.co>
2025-01-08 15:55:56 +0000

    Make sure script can be called from repo root

--- dockers/llm.vdb.service/makeDocker.sh
+++ dockers/llm.vdb.service/makeDocker.sh
@@ -6,6 +6,9 @@ set -e
 CREATE_VECTOR_DB_REPO=$1
 CREATE_VECTOR_DB_TAG=$2
 
+# Get the directory where the script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 echo ""
 echo "Building docker for vectordb creation"
 docker buildx build --platform=linux/amd64 --load \

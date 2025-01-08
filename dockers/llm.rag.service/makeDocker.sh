53424b3bb6cd836f18c425ee4a312c2862a1418e
Maciej Urbański <maciek@elotl.co>
2025-01-08 15:55:51 +0000

    Make sure script can be called from repo root

--- dockers/llm.rag.service/makeDocker.sh
+++ dockers/llm.rag.service/makeDocker.sh
@@ -6,7 +6,14 @@ set -e
 SERVE_RAG_LLM_REPO=$1
 SERVE_RAG_LLM_TAG=$2
 
+# Get the directory where the script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 echo ""
 echo "Building docker for rag+llm service"
-docker buildx build --platform=linux/amd64 --load -f ./Dockerfile -t ${SERVE_RAG_LLM_REPO}:${SERVE_RAG_LLM_TAG} .
-docker push ${SERVE_RAG_LLM_REPO}:${SERVE_RAG_LLM_TAG}
+docker buildx build --platform=linux/amd64 --load \
+  -f "${SCRIPT_DIR}/Dockerfile" \
+  -t ${SERVE_RAG_LLM_REPO}:${SERVE_RAG_LLM_TAG} \
+  "${SCRIPT_DIR}"
+
+# docker push ${SERVE_RAG_LLM_REPO}:${SERVE_RAG_LLM_TAG}

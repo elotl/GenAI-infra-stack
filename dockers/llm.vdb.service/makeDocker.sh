4693bbd9073a47c1f90f5b0b8c7af346e6f5855d
Maciek Urbański <maciek.urbanski1@gmail.com>
2025-01-08 16:00:03 +0000

    Fix create and publish images workflow (#30)
    * Make sure script can be called from repo root
    
    * Make sure script can be called from repo root
    
    * Make sure script can be called from repo root

--- dockers/llm.vdb.service/makeDocker.sh
+++ dockers/llm.vdb.service/makeDocker.sh
@@ -6,7 +6,14 @@ set -e
 CREATE_VECTOR_DB_REPO=$1
 CREATE_VECTOR_DB_TAG=$2
 
+# Get the directory where the script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 echo ""
 echo "Building docker for vectordb creation"
-docker buildx build --platform=linux/amd64 --load -f ./Dockerfile -t ${CREATE_VECTOR_DB_REPO}:${CREATE_VECTOR_DB_TAG} .
+docker buildx build --platform=linux/amd64 --load \
+  -f "${SCRIPT_DIR}/Dockerfile" \
+  -t ${CREATE_VECTOR_DB_REPO}:${CREATE_VECTOR_DB_TAG} \
+  "${SCRIPT_DIR}"
+
 docker push ${CREATE_VECTOR_DB_REPO}:${CREATE_VECTOR_DB_TAG}

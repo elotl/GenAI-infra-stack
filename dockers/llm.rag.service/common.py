b7e74159e5a72c9f5b415bb20f8ff7be439d2e0b
Selvi Kadirvel <selvikadirvel@gmail.com>
2025-01-16 10:27:48 -0800

    Update logging in RAG LLM

--- dockers/llm.rag.service/common.py
+++ dockers/llm.rag.service/common.py
@@ -7,7 +7,7 @@ def format_context(results: List[Dict[str, Any]]) -> str:
     context_parts = []
 
     for result in results:
-        # TODO: make metadata keyes configurable
+        # TODO: make metadata keys configurable
         ticket_metadata = result.metadata
         ticket_content = result.page_content
 
@@ -25,7 +25,7 @@ def remove_context_from_answer(generated_answer: str):
     Args:
         text (str): the generated answer to remove context from
     Returns:
-        str: Cleaned answer with all content before the context title
+        str: Cleaned answer with all content before the context label
     """
     if not generated_answer:  # Handle empty text
         return ""
@@ -35,21 +35,21 @@ def remove_context_from_answer(generated_answer: str):
     # Split text at the token and take only the content before it
     if context_title in generated_answer:
         answer = generated_answer.split(context_title, 1)[0]
-        logging.info(f"Context removed from generated answer: {answer}")
+        logging.info(f"Context seems to have been included in the generated answer and it has been removed: {answer}")
 
     return answer.strip()
 
 
 def get_answer_with_settings(question, retriever, client, model_id, max_tokens, model_temperature, system_prompt):
     docs = retriever.invoke(input=question)
-    logging.info(
-        "Number of relevant documents retrieved and that will be used as context for query: ",
-        len(docs),
-    )
+    num_of_docs = len(docs)
+    logging.info(f"Number of relevant documents retrieved and that will be used as context for query: {num_of_docs}")
 
+    logging.info(f"Relevant docs retrieved from Vector store: {docs}")
     context = format_context(docs)
+    logging.info(f"Context after formatting: {context}")
 
-    print("Calling chat completions for JSON model...")
+    logging.info("Calling chat completions for JSON model...")
     completions = client.chat.completions.create(
         model=model_id,
         messages=[
@@ -64,15 +64,16 @@ def get_answer_with_settings(question, retriever, client, model_id, max_tokens, 
         stream=False,
     )
 
-    # force remove context for the cases when the LLM appends context to the generated answer
     generated_answer = completions.choices[0].message.content
-    answer = remove_context_from_answer(generated_answer)
 
-    answer = {
+    # force remove context for the cases when the LLM appends context to the generated answer
+    answer = remove_context_from_answer(generated_answer)
+    logging.info(f"Generated answer (after cleanup): {answer}")
+
+    answerToUI = {
         "answer": answer,
         "relevant_tickets": [r.metadata["ticket"] for r in docs],
         "sources": [r.metadata["source"] for r in docs],
         "context": context,  # TODO: if this is big consider logging context here and sending some reference id to UI
     }
-    logging.info("Received answer: ", answer)
-    return answer
+    return answerToUI

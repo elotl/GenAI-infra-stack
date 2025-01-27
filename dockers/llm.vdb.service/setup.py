71ad72fe124b15903f03b1125c487e49f15ff68e
Maciej Urbański <maciek@elotl.co>
2025-01-27 19:34:24 +0000

    Cleanup docker and project file

--- dockers/llm.vdb.service/setup.py
+++ dockers/llm.vdb.service/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="createvectordb",
     version="0.1.0",
-    py_modules=["common", "createvectordb", "createvectordb_jira_csv_local"],
+    py_modules=["common", "config", "createvectordb", "s3_utils", "service"],
 )

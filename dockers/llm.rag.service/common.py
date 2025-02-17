fa9d6aab9cb3cbe07bdb8e3578657564a2e4c00f
Selvi Kadirvel <selvikadirvel@gmail.com>
2025-02-17 13:37:40 -0800

    Text to SQL agent for CSV data

--- dockers/llm.rag.service/common.py
+++ dockers/llm.rag.service/common.py
@@ -1,7 +1,68 @@
-from typing import Any, Dict, List
-
+import re
 import logging
+from typing import Any, Dict, List
+from typing_extensions import Annotated
+from typing_extensions import TypedDict
+from operator import itemgetter
 
+from sqlalchemy import create_engine
+
+from transformers import AutoTokenizer
+
+from langchain_community.chat_models import ChatOpenAI
+from langchain import hub
+from langchain_community.utilities import SQLDatabase
+from langchain.chains import create_sql_query_chain
+from langchain_community.utilities import SQLDatabase
+from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
+
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+
+import logging.config
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'elotl-qa-in-a-box.log',
+            'formatter': 'default',
+        },
+        'stdout': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+        },
+    },
+    'loggers': {
+        'ElotlQAInABoxLogger': {
+            'handlers': ['file', 'stdout'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+logging.config.dictConfig(LOGGING_CONFIG)
+logger = logging.getLogger('ElotlQAInABoxLogger')
+
+class State(TypedDict):
+    question: str
+    query: str
+    result: str
+    answer: str
+
+MODEL_MAX_CONTEXT_LEN = 8192
+delta = 50 # value by which we keep the prompt len less than the model context len  
 
 def format_context(results: List[Dict[str, Any]]) -> str:
     """Format search results into context for the LLM"""
@@ -38,7 +99,7 @@ def trim_answer(generated_answer: str, label_separator: str) -> str:
     # Split text at the token and take only the content before it
     if label_separator in generated_answer:
         answer = generated_answer.split(label_separator, 1)[0]
-        logging.info(f"Label separator: {label_separator} seems to have been included in the generated answer and it has been removed: {answer}")
+        logger.info(f"Label separator: {label_separator} seems to have been included in the generated answer and it has been removed: {answer}")
 
     return answer.strip()
 
@@ -46,13 +107,13 @@ def trim_answer(generated_answer: str, label_separator: str) -> str:
 def get_answer_with_settings(question, retriever, client, model_id, max_tokens, model_temperature, system_prompt):
     docs = retriever.invoke(input=question)
     num_of_docs = len(docs)
-    logging.info(f"Number of relevant documents retrieved and that will be used as context for query: {num_of_docs}")
+    logger.info(f"Number of relevant documents retrieved and that will be used as context for query: {num_of_docs}")
 
-    logging.info(f"Relevant docs retrieved from Vector store: {docs}")
+    logger.info(f"Relevant docs retrieved from Vector store: {docs}")
     context = format_context(docs)
-    logging.info(f"Context after formatting: {context}")
+    logger.info(f"Context after formatting: {context}")
 
-    logging.info("Calling chat completions for JSON model...")
+    logger.info("Calling chat completions for JSON model...")
     try:
         completions = client.chat.completions.create(
             model=model_id,
@@ -69,7 +130,7 @@ def get_answer_with_settings(question, retriever, client, model_id, max_tokens, 
         )
     except Exception as e:
         # Handle any error
-        logging.error(f"An unexpected error occurred: {e}")
+        logger.error(f"An unexpected error occurred: {e}")
         errorToUI = {
             "answer": f"Please try another question. Received error from LLM invocation: {e}",
             "relevant_tickets": [],
@@ -77,14 +138,115 @@ def get_answer_with_settings(question, retriever, client, model_id, max_tokens, 
             "context": context,
         }
         return errorToUI
-
+    
     generated_answer = completions.choices[0].message.content
+
+    answer = postprocess_hallucinations(generated_answer)
+
+    answerToUI = {
+        "answer": answer,
+        "relevant_tickets": [r.metadata["ticket"] for r in docs],
+        "sources": [r.metadata["source"] for r in docs],
+        "context": context,  # TODO: if this is big consider logger context here and sending some reference id to UI
+    }
+    return answerToUI
+
+
+def get_sql_answer(question, model_id, max_tokens, model_temperature, llm_server_url):
+    
+    logger.info("Invoking text-to-sql question-answer search")
+    try:
+        llm = ChatOpenAI(
+            model=model_id,
+            temperature=model_temperature,
+            openai_api_base=llm_server_url,
+            openai_api_key="unused-for-self-hosted-llms",
+            max_tokens=max_tokens
+        )        
+
+        logger.info("Loading SQL DB")
+        engine = create_engine("sqlite:///zendesk.db")
+
+        # TO DO 
+        # CREATE the SQL DB from the CSV - to be done only once.
+        #df.to_sql("zendesk", engine, index=False)
+
+        logger.info("Check that the SQL data can be accessed from the DB via querying")
+        db = SQLDatabase(engine=engine)
+        logger.info(f"DB dialect is: {db.dialect}")
+        logger.info(f"Usable table names: {db.get_usable_table_names()}")
+        logger.info(f"Table info: {db.get_table_info(["zendesk"])}")
+
+        # Use ready-made prompts from Langchain hub
+        # Later we can customize this for different datasets
+        # Prompt template to convert NL question to SQL was rertrieved from
+        # langchain hub and customized.
+        query_prompt_template = prompt_template_for_text_to_sql()
+
+        # Alternatively uncomment below to use prompt template from hub 
+        # without customization    
+        # query_prompt_template = hub.pull("langchain-ai/sql-query-system-prompt")
+        # assert len(query_prompt_template.messages) == 1
+        
+        logger.info("Prompt template for text-to-sql conversion:" 
+                     "{query_prompt_template.messages[0]}")
+
+    except Exception as e:
+        logger.error(f"An unexpected error occurred: {e}")
+        errorToUI = {
+            "answer": f"Please try another question. Received error from LLM invocation: {e}",
+            "relevant_tickets": [],
+            "sources": [],
+            "context": "",
+        }
+        return errorToUI
+
+    # send SQL query and response to LLM and get a natural language answer
+    # TODO avoid calling text to SQL conversion twice
+    state: State = {
+        "question": question,
+        "query": write_query({"question": question}, query_prompt_template, llm, db),
+        "result": execute_query(write_query({"question": question}, query_prompt_template, llm, db), db),
+    }
+    generated_answer = convert_sql_result_to_nl(state, model_id, llm)
+    answer = postprocess_hallucinations(generated_answer['answer'])
+    
+    answerToUI = {
+        "answer": answer,
+        "relevant_tickets": [],
+        "sources": [],
+        "context": "",  # TODO: if this is big consider logger context here and sending some reference id to UI
+    }
+    return answerToUI
+
+def prompt_template_for_text_to_sql():
+
+    query_prompt_template = PromptTemplate.from_template(
+        "Given an input question, create a syntactically correct "
+        "{dialect} query to run to help find the answer." 
+        "Unless the user specifies in his question a specific " 
+        "number of examples they wish to obtain, always limit " 
+        "your query to at most {top_k} results. You can order "
+        "the results by a relevant column to return the most " 
+        "interesting examples in the database. Never query for " 
+        "all the columns from a specific table, only ask for a " 
+        "few relevant columns given the question. Pay attention " 
+        "to use only the column names that you can see in the schema "
+        "description. Be careful to not query for columns that do "
+        "not exist. Also, pay attention to which column is in " 
+        "which table. Only use the following tables: {table_info}."
+        "If there is a ticket ID in the question, ensure that you maintain "
+        "the exact ticket ID in the query."
+        "Question: {input}")
+    return query_prompt_template
+
+def postprocess_hallucinations(generated_answer: str) -> str:
 
     # Handle common hallucinations observed:
     #    1. Added-Context hallucination
     #    2. Added-Question hallucination
     #    3. Added-Context hallucination just labelled as "Content" (instead of Context like 1)
-    logging.info(f"Removing any observed hallucinations in the generated answer: {generated_answer}")
+    logger.info(f"Removing any observed hallucinations in the generated answer: {generated_answer}")
     labels_to_trim = ["<|im_end|>", "Context:", "Question:", "Content:"]
     answer = generated_answer
 
@@ -94,12 +256,136 @@ def get_answer_with_settings(question, retriever, client, model_id, max_tokens, 
 
     answer = answer.replace("<|im_start|>", "")
 
-    logging.info(f"Answer (after cleanup): {answer}")
+    logger.info(f"Answer (after cleanup): {answer}")
 
-    answerToUI = {
-        "answer": answer,
-        "relevant_tickets": [r.metadata["ticket"] for r in docs],
-        "sources": [r.metadata["source"] for r in docs],
-        "context": context,  # TODO: if this is big consider logging context here and sending some reference id to UI
-    }
-    return answerToUI
+    return answer
+
+
+def createdb():
+
+    engine = create_engine("sqlite:///zendesk.db")
+
+    # CREATE the SQL DB from the CSV - to be done only once.
+    #df.to_sql("zendesk", engine, index=False)
+
+    """Check that the SQL data can be accessed from the DB via querying"""
+
+    db = SQLDatabase(engine=engine)
+    print("DB dialect is:", db.dialect)
+    print("Usable table names: ", db.get_usable_table_names())
+    print(db.get_table_info(db.get_usable_table_names()))
+    print(db.run("SELECT COUNT(*) FROM zendesk WHERE assignee_name LIKE 'David Levey';"))
+
+
+class QueryOutput(TypedDict):
+    """Generated SQL query."""
+
+    query: Annotated[str, ..., "Syntactically valid SQL query."]
+
+
+def extract_sql_query(message: str) -> str:
+    pattern = r'```sql\n(.*?)\n```'
+    match = re.search(pattern, message, re.DOTALL)
+
+    if match:
+        return match.group(1) 
+    else:
+        sql_query = postprocess_hallucinations(message)
+
+    return sql_query
+
+
+def write_query(state: State, query_prompt_template, llm, db):
+    """Generate SQL query to fetch information."""
+    prompt = query_prompt_template.invoke(
+        {
+            "dialect": db.dialect,
+            "top_k": 10,
+            "table_info": db.get_table_info(),
+            "input": state["question"],
+        }
+    )
+    # structure output wasn't implemented for the RUBRA-phi3 model so had to move to the 
+    # raw llm invoke. If we can move to a different function-calling LLM, we can uncomment
+    # this.
+    #structured_llm = llm.with_structured_output(QueryOutput)
+    #result = structured_llm.invoke(prompt)
+
+    logger.info(f"Prompt for SQL query generation: {prompt}")
+    result = llm.invoke(prompt)
+
+    '''
+    # post-process LLM output to extract only the query, handles both cases when
+    # returned output is of these forms:
+    # a) "sql: select title from employee limit 10"
+    # b) SELECT subject FROM tickets WHERE ticket_id = 685490;<|im_end|>
+        <|im_start|>user>
+        Question: What are the details of the tickets with the highest priority?<|im_end|>
+        <|im_start|>assistant>
+        SELECT subject, FROM tickets ORDER BY priority DESC LIMIT 10;<|im_end|>
+    '''
+    sql_query = extract_sql_query(result.content)
+
+    logger.info(f"Extracted SQL query: {sql_query}")
+    
+    return {"query": sql_query}
+
+
+# Executes a SQL query against the provided database
+def execute_query(state: State, db):
+    """Execute SQL query."""
+    execute_query_tool = QuerySQLDataBaseTool(db=db)
+    return {"result": execute_query_tool.invoke(state["query"])}
+
+# Trims a string to fit within a given token limit using a model-specific tokenizer.
+def trim_text_by_tokens(text: str, model_id: str, token_limit: int) -> str:
+ 
+    # Load a tokenizer that is specific to the model
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    
+    tokens = tokenizer(text)["input_ids"]
+    
+    # If within the limit, return the original text
+    if len(tokens) <= token_limit:
+        return text
+    
+    # Trim the tokens to the allowed limit
+    trimmed_tokens = tokens[:token_limit]
+    
+    # Decode back into text
+    trimmed_text = tokenizer.decode(trimmed_tokens, skip_special_tokens=True)
+    
+    return trimmed_text
+
+# Answer user's question in natural language using SQL query and SQL query results from the 
+# database as context. 
+def convert_sql_result_to_nl(state: State, model_id, llm):
+    
+    domainExpertInstructions = "In the provided SQL table, each entry or row refers to a ticket and not to a customer." 
+    " The column titled requester is also referred to as the customer or submitter or client."
+    " The column titled all_comments can also be referred to as responses or resolution or details"
+    
+    prompt = (
+        "You are a customer support ticket expert. Given the following user question, corresponding SQL query, "
+        "and SQL result, answer the user's question."
+        "Do not make any references to the SQL query or the SQL result in your answer." 
+        + domainExpertInstructions + 
+        f'Question: {state["question"]}\n'
+        f'SQL Query: {state["query"]}\n'
+        f'SQL Result: {state["result"]}'
+    )
+    logger.info(f"Prompt for SQL result to NL conversion: {prompt}. Prompt length: {len(prompt)}")
+
+    # Prompt length has to be smaller than model max because of errors like this:
+    # This model's maximum context length is 8192 tokens. However, you requested 8220 
+    # tokens in the messages, Please reduce the length of the messages.", 
+    # 'type': 'BadRequestError',
+    # 'param': None, 'code': 400}
+    PROMPT_TRIM_LENGTH = MODEL_MAX_CONTEXT_LEN - delta
+    trimmed_prompt = trim_text_by_tokens(prompt, model_id, PROMPT_TRIM_LENGTH) 
+    logger.info("Trimmed prompt:", trimmed_prompt)
+
+    response = llm.invoke(trimmed_prompt)
+    logger.info("LLM generated NL answer to user question:", response.content)
+
+    return {"answer": response.content}

a1b7db9f069549a3efb1fe70f61ec99f60b898f2
Selvi Kadirvel <selvik@users.noreply.github.com>
2025-03-05 18:51:40 -0800

    Text to SQL querying for structured data (#69)
    * Updates to convert CSV to SQL lite DB for query execution
    
    * Add PV to RAG LLM service for mounting SQL DB for text-to-sql search
    
    * Convert zendesk JSON to CSV
    
    * Change QuestionType to SearchType, Update Dockerfile with DB
    
    * Add python deps for text-to-sql in Dockerfile
    
    * Add PV and PVC for support ticket DB for RAG LLM deployment
    
    * Fix retriever creation: Only for VECTOR search type
    
    * Comments cleanup, Fix SQL DB addition
    
    * Text to SQL querying for structured data
    
    - Adds Text to SQL capability for answering questions about user's structured data.
    - LLM inferencing is used in two ways in this new workflow:
       1) Converting the user's natural language question into a SQL query
       2) Converting the SQL results into a natural language answer that will be returned to the user
    - This PR uses Langchain's text-to-SQL APIs which require the use of an LLM that has function-calling (or tool-calling) ability. So this PR needs to be used with an LLM like the RubraAI's Phi-3 model: [https://huggingface.co/rubra-ai/Phi-3-mini-128k-instruct](https://huggingface.co/rubra-ai/Phi-3-mini-128k-instruct). This model takes the base Phi3 model and fine-tunes it for function calling.
    - This PR expects the structured data to be mounted to the RAG LLM deployment via a Persistent volume from S3. Instructions on doing this will be added to the [Install docs](https://github.com/elotl/GenAI-infra-stack/blob/main/docs/install.md).
    - This new env variable needs to be defined to invoke this feature:
    `export SEARCH_TYPE=SQL`
     In this PR, default search is being set to SQL temporarily. A follow up PR will allow an incoming question to be automatically classified and the appropriate search type: SQL or VECTOR to be picked at runtime.
    - This feature is only available on EKS and will be extended to other cloud providers in follow-up PRs.
    
    1. Run the RubraAI model in the GenAI infra stack and port-forward the LLM query endpoint:
    
    ```
     % kubectl get svc
    NAME                                        TYPE        CLUSTER-IP       EXTERNAL-IP   PORT(S)                                         AGE
    kuberay-operator                            ClusterIP   10.100.164.141   <none>        8080/TCP                                        10d
    kubernetes                                  ClusterIP   10.100.0.1       <none>        443/TCP                                         10d
    llm-model-serve-head-svc                    ClusterIP   10.100.235.95    <none>        10001/TCP,8265/TCP,6379/TCP,8080/TCP,8000/TCP   17h
    llm-model-serve-raycluster-fhc7j-head-svc   ClusterIP   10.100.202.89    <none>        10001/TCP,8265/TCP,6379/TCP,8080/TCP,8000/TCP   18h
    llm-model-serve-serve-svc                   ClusterIP   10.100.63.8      <none>        8000/TCP                                        17h
    ```
    
    ```
    selvik@Selvis-MacBook-Pro pynb % kubectl  port-forward svc/llm-model-serve-serve-svc  8080:8000
    Forwarding from 127.0.0.1:8080 -> 8000
    Forwarding from [::1]:8080 -> 8000
    ```
    
    2. Run the local version of the text-to-sql script:
    `python serverragllm_zendesk_csv_sql_local.py
    `
    
    3. Send a question to this local endpoint:
    `
    % cd GenAI-infra-stack/scripts/query`
    
    `% python query_private_data.py`
    
    `Type your query here: How many tickets are there?`
    
    `Answer: {'answer': 'There are 123 tickets.', 'relevant_tickets': ['n/a'], 'sources': ['n/a'], 'context': ''}
    `

---
+++ dockers/llm.rag.service/serverragllm_zendesk_csv_sql_local.py
@@ -0,0 +1,143 @@
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "faiss-cpu",
+#     "fastapi",
+#     "langchain-community",
+#     "langchain-huggingface",
+#     "openai",
+#     "uvicorn",
+# ]
+# ///
+
+import os
+import pickle
+import sys
+import logging
+import uvicorn
+
+from functools import partial
+from typing import Union
+
+import click
+from fastapi import FastAPI
+from openai import OpenAI
+
+from common import get_answer_with_settings
+from common import get_sql_answer
+from enum import Enum
+
+class SearchType(Enum):
+    SQL = 1
+    VECTOR = 2
+
+def setup(
+        file_path: str,
+        relevant_docs: int,
+        llm_server_url:str,
+        model_id: str,
+        max_tokens: int,
+        model_temperature: float,
+):
+    app = FastAPI()
+
+    # TO DO: Add question classification block
+
+    # For now, hard-coding question type to aggregation
+    search_type = SearchType.SQL
+
+    match search_type:
+        case SearchType.SQL: 
+            logging.info("Handling search type: SQL")
+
+            get_answer = partial(
+                get_sql_answer,
+                model_id=model_id,
+                max_tokens=max_tokens,
+                model_temperature=model_temperature,
+                llm_server_url=llm_server_url,
+            )
+        case SearchType.VECTOR: 
+            logging.info("Handling search type: VECTOR")
+
+            # Load the object from the pickle file
+            with open(file_path, "rb") as file:
+                print("Loading Vector DB...\n")
+                vectorstore = pickle.load(file)
+
+            # Retriever configuration parameters reference:
+            # https://python.langchain.com/api_reference/community/vectorstores/langchain_community.vectorstores.faiss.FAISS.html#langchain_community.vectorstores.faiss.FAISS.as_retriever
+            retriever = vectorstore.as_retriever(search_kwargs={"k": relevant_docs})
+            print("Created Vector DB retriever successfully. \n")
+
+            print("Creating an OpenAI client to the hosted model at URL: ", llm_server_url)
+            try:
+                client = OpenAI(base_url=llm_server_url, api_key="na")
+            except Exception as e:
+                print("Error creating client:", e)
+                sys.exit(1)
+
+            jira_system_prompt = """You are a specialized support ticket assistant. Format your responses following these rules:
+                        1. Answer the provided question only using the provided context.
+                        2. Provide a clear, direct and factual answer
+                        3. Include relevant technical details when present
+                        4. If the information is outdated, mention when it was last updated
+                        """
+
+            get_answer = partial(
+                get_answer_with_settings,
+                retriever=retriever,
+                client=client,
+                model_id=model_id,
+                max_tokens=max_tokens,
+                model_temperature=model_temperature,
+                system_prompt=jira_system_prompt,
+            )
+
+
+    @app.get("/answer/{question}")
+    def read_item(question: Union[str, None] = None):
+        print(f"Received question: {question}")
+        answer = get_answer(question)
+        return {"question": question, "answer": answer}
+
+    return app
+
+
+MICROSOFT_MODEL_ID = "microsoft/Phi-3-mini-4k-instruct"
+MOSAICML_MODEL_ID = "mosaicml/mpt-7b-chat"
+RELEVANT_DOCS_DEFAULT = 2
+MAX_TOKENS_DEFAULT = 350
+MODEL_TEMPERATURE_DEFAULT = 0.00
+
+
+file_path = os.getenv("FILE_PATH")
+if not file_path:
+    print("Please provide the DB file path")
+
+logging.info("Setting LLM setup parameters like LLM server URL, model id, model temperature...")
+relevant_docs = int(os.getenv("RELEVANT_DOCS", RELEVANT_DOCS_DEFAULT))
+
+# LLM server URL if using ollama hosting locally
+# llm_server_url = os.getenv("LLM_SERVER_URL", "http://localhost:11434/v1")
+# model_id = os.getenv("MODEL_ID", "llama2")
+
+# LLM server URL if using k8s elotl hosting + port-forwarding 
+llm_server_url = os.getenv("LLM_SERVER_URL", "http://localhost:8080/v1")
+model_id = os.getenv("MODEL_ID", "rubra-ai/Phi-3-mini-128k-instruct")
+
+max_tokens = int(os.getenv("MAX_TOKENS", MAX_TOKENS_DEFAULT))
+model_temperature = float(os.getenv("MODEL_TEMPERATURE", MODEL_TEMPERATURE_DEFAULT))
+
+app = setup(file_path, relevant_docs, llm_server_url, model_id, max_tokens, model_temperature)
+
+@click.command()
+@click.option("--host", default="127.0.0.1", help="Host for the FastAPI server (default: 127.0.0.1)")
+@click.option("--port", type=int, default=8000, help="Port for the FastAPI server (default: 8000)")
+def run(host, port):
+    # Serve the app using Uvicorn
+    uvicorn.run("serverragllm_zendesk_csv_sql_local:app", host=host, port=port, reload=True)
+
+
+if __name__ == "__main__":
+    run()
